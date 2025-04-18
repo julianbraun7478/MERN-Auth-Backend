1befc74035a0150c2d18b410fe5a342a93b61722
FullStackDev001 <162185577+firasbk7770@users.noreply.github.com>
2025-04-17 19:44:58 -0700

    modified

--- config/db.js
+++ config/db.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    mongoose.connect('mongodb+srv://username:root@cluster0.tcbgdea.mongodb.net/authlogin', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      })
+    mongoose.connect('mongodb+srv://username:root@cluster0.tcbgdea.mongodb.net/authlogin')
       .then(() => {
         console.log("mongodb+srv://user:root@cluster0.qcowr2v.mongodb.net/test?retryWrites=true&w=majority")
       })
